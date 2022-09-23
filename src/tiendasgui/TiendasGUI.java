7aca08a4fd5b47493bd996e17ada92ef85703b2a
Jairo-mirand <jairomiranda112233@gmail.com>
2022-09-23 15:22:35 -0500

    enlazar proyecto

---
+++ src/tiendasgui/TiendasGUI.java
@@ -0,0 +1,12 @@
+package tiendasgui;
+
+import tiendasgui.vista.VCliente;
+
+public class TiendasGUI {
+    public static void main(String[] args) {
+        VCliente pantallaCliente= new VCliente();
+        pantallaCliente.setVisible(true);
+        pantallaCliente.setLocationRelativeTo(null);
+    }
+    
+}
